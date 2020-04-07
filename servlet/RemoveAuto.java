ed827421d71ef87cc9361c0ba74a423f302d9650
Roberto Piu <60385159+roberto20007113@users.noreply.github.com>
2020-04-07 16:33:28 +0200

    Create RemoveAuto.java

---
+++ servlet/RemoveAuto.java
@@ -0,0 +1,44 @@
+package servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import database.Database;
+import model.Auto;
+import model.Categoria;
+
+@WebServlet("/removeauto")
+public class RemoveAuto extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+    public RemoveAuto() {
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		request.setAttribute("listaAuto", Database.getInstance().getListaAuto());
+		
+		request.getServletContext().getRequestDispatcher("/jsp/header.jsp").include(request, response);
+		request.getServletContext().getRequestDispatcher("/jsp/navbar.jsp").include(request, response);
+		request.getServletContext().getRequestDispatcher("[pagina lista auto]").include(request, response);
+		request.getServletContext().getRequestDispatcher("/jsp/footer.jsp").include(request, response);
+		
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		if(request.getAttribute("utente")!=null || request.getAttribute("email_admin")!=null) {
+			int id = Integer.parseInt(request.getParameter("[id auto da rimuovere]"));
+			Auto a = Database.getInstance().getAutoById(id);
+			a.setDisponibilita((byte) 2);
+			Database.getInstance().updateAuto(a);
+			response.sendRedirect("/removeauto");
+		}else {
+			response.sendRedirect("[home]");
+		}
+	}
+}
