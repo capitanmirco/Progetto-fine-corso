e0e00763ca7f1c4b8f90d6dcd8ad2b8bf26acdb8
davidpintilie <60385190+davidpintilie@users.noreply.github.com>
2020-04-08 11:26:34 +0200

    Update AggiungiClienti.java

--- servlet/AggiungiClienti.java
+++ servlet/AggiungiClienti.java
@@ -9,20 +9,16 @@ import javax.servlet.http.HttpServletResponse;
 
 import database.Database;
 import model.Cliente;
-
 @WebServlet(name="aggiungiclienti", urlPatterns= {"/aggiungiclienti"})
+//@WebServlet("/aggiungiclienti")
 public class AggiungiClienti extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
-    public AggiungiClienti() {
-        super();
-        
-    }
-
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		Cliente c = (Cliente) request.getAttribute("cliente");
-		Database.getInstance().addCliente(c);
+		Database.getIstance().addCliente(c);
+		System.out.println("ok cliente aggiunto");
 	}
 
 }
+
