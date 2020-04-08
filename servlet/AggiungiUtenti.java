f9dbb9abad3969557593c1dd26851de4cde0b549
davidpintilie <60385190+davidpintilie@users.noreply.github.com>
2020-04-08 11:27:44 +0200

    Add files via upload

---
+++ servlet/AggiungiUtenti.java
@@ -0,0 +1,22 @@
+package servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import database.Database;
+import model.Utente;
+
+@WebServlet(name = "aggiungiutenti", urlPatterns = { "/aggiungiutenti" })
+public class AggiungiUtenti extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		Utente u = (Utente) request.getAttribute("utente");
+		Database.getIstance().addUtente(u);
+		System.out.println("ok utente aggiunto");
+	}
+}
