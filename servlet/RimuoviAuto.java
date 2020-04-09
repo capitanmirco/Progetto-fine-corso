aae5b8623e28f195f94d292fc96e8a132417741c
federicomadeddu <60399073+federicomadeddu@users.noreply.github.com>
2020-04-09 11:15:06 +0200

    Update RimuoviAuto.java

--- servlet/RimuoviAuto.java
+++ servlet/RimuoviAuto.java
@@ -1,15 +1,17 @@
 package servlet;
 
 import java.io.IOException;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import database.Database;
 import model.Auto;
-import model.Categoria;
 
 @WebServlet("/rimuoviauto")
 public class RimuoviAuto extends HttpServlet {
@@ -20,25 +22,47 @@ public class RimuoviAuto extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		request.setAttribute("listaAuto", Database.getInstance().getListaAuto());
+		HttpSession session = request.getSession();
 		
-		request.getServletContext().getRequestDispatcher("/jsp/header.jsp").include(request, response);
-		request.getServletContext().getRequestDispatcher("/jsp/navbar.jsp").include(request, response);
-		request.getServletContext().getRequestDispatcher("[pagina lista auto]").include(request, response);
-		request.getServletContext().getRequestDispatcher("/jsp/footer.jsp").include(request, response);
+		List<Auto> listaAuto = Database.getInstance().getListaAuto();
+		
+		for(int i=0; i<listaAuto.size(); i++) {
+			
+				if(listaAuto.get(i).getDisponibilita()==2) {
+					listaAuto.remove(i);
+				}
+				
+		}
+		
+		request.setAttribute("listaAuto", listaAuto);
+		
+		byte disponibilita = 2;
+		
+		if(session.getAttribute("utente")!=null || session.getAttribute("email_admin")!=null) {
+			
+		
+			request.getServletContext().getRequestDispatcher("/jsp/header.jsp").include(request, response);
+			request.getServletContext().getRequestDispatcher("/jsp/navbar.jsp").include(request, response);
+			request.getServletContext().getRequestDispatcher("/jsp/lista.jsp").include(request, response);
+			request.getServletContext().getRequestDispatcher("/jsp/footer.jsp").include(request, response);
+			
+			if(request.getParameter("remove")!=null) {
+				int id = Integer.parseInt(request.getParameter("remove"));
+				Auto a = Database.getInstance().getAutoById(id);
+				a.setDisponibilita(disponibilita);
+				Database.getInstance().updateAuto(a);
+				response.sendRedirect("rimuoviauto");
+			}
+
+		}
+		
+		else {
+			response.sendRedirect("home");
+		}
 		
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		if(request.getAttribute("utente")!=null || request.getAttribute("email_admin")!=null) {
-			int id = Integer.parseInt(request.getParameter("[id auto da rimuovere]"));
-			Auto a = Database.getInstance().getAutoById(id);
-			a.setDisponibilita((byte) 2);
-			Database.getInstance().updateAuto(a);
-			response.sendRedirect("/rimuoviauto");
-		}else {
-			response.sendRedirect("[home]");
-		}
 	}
+	
 }
