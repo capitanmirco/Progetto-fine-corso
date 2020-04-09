e1ad89f7407d44059ecdc6fb8b58a39facc4dfc3
federicomadeddu <60399073+federicomadeddu@users.noreply.github.com>
2020-04-09 12:37:05 +0200

    Update ModificaAuto.java

--- servlet/ModificaAuto.java
+++ servlet/ModificaAuto.java
@@ -11,7 +11,6 @@ import javax.servlet.http.HttpSession;
 import database.Database;
 import model.Auto;
 import model.Categoria;
-import model.Utente;
 
 
 @WebServlet("/ModificaAuto")
@@ -29,18 +28,23 @@ public class ModificaAuto extends HttpServlet {
 		
 		HttpSession session=request.getSession();
 		if (session.getAttribute("utente")!=null || session.getAttribute("email_admin")!=null) {
-			
-		
-			//prendo l'id che mi passer‡ il catalogo cliccando su modifica auto e creo un'auto
-			int id=Integer.parseInt(request.getParameter("modifica"));
-			Auto a=Database.getInstance().getAutoById(id);
-			request.setAttribute("auto", a);
 
-			request.getServletContext().getRequestDispatcher("/jsp/header.jsp").include(request,response);
-			request.getServletContext().getRequestDispatcher("/jsp/navbar.jsp").include(request,response);
-			request.getServletContext().getRequestDispatcher("/jsp/formAuto.jsp").include(request,response);
-			request.getServletContext().getRequestDispatcher("/jsp/footer.jsp").include(request,response);
+			//prendo l'id che mi passer√† il catalogo cliccando su modifica auto e creo un'auto
+			if(request.getParameter("modifica")!=null) {
+				
+				int id=Integer.parseInt(request.getParameter("modifica"));
+				Auto a=Database.getInstance().getAutoById(id);
+				request.setAttribute("auto", a);
+				
+			}
+			
+				request.getServletContext().getRequestDispatcher("/jsp/header.jsp").include(request,response);
+				request.getServletContext().getRequestDispatcher("/jsp/navbar.jsp").include(request,response);
+				request.getServletContext().getRequestDispatcher("/jsp/formAuto.jsp").include(request,response);
+				request.getServletContext().getRequestDispatcher("/jsp/footer.jsp").include(request,response);
+
 		}
+		
 		else {
 			response.sendRedirect("home");
 		}
@@ -51,27 +55,32 @@ public class ModificaAuto extends HttpServlet {
 		
 		HttpSession session=request.getSession();
 		if (session.getAttribute("utente")!=null || session.getAttribute("email_admin")!=null) {
-			//mi creo un'auto e una categoria per andare a creare un noleggio e inserirlo nel database
-			Auto a=new Auto();
+			//mi creo un'auto 
 			int id=Integer.parseInt(request.getParameter("id"));
+			
+			Auto a= Database.getInstance().getAutoById(id);  
+			
 			double cilindrata=Double.parseDouble(request.getParameter("cilindrata"));
-			byte disponibilita=Byte.parseByte(request.getParameter("disponibilita"));
-			int idCategoria=Integer.parseInt(request.getParameter("idCategoria"));
+			int idCategoria=Integer.parseInt(request.getParameter("categoria"));
+			
 			Categoria c=Database.getInstance().getCategoriaById(idCategoria);
-			a.setIdAuto(id);
+			
+			
 			a.setCilindrata(cilindrata);
 			a.setColore(request.getParameter("colore"));
-			a.setDisponibilita(disponibilita);
 			a.setMarca(request.getParameter("marca"));
 			a.setModello(request.getParameter("modello"));
 			a.setTarga(request.getParameter("targa"));
 			a.setCategoria(c);
 			Database.getInstance().updateAuto(a);
+			System.out.println("auto aggiornata");
 			response.sendRedirect("catalogo");
 		}
+		
 		else {
 			response.sendRedirect("home");
 		}
+		
 	}
 
 }
