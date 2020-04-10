f9a1251efbc7efca8dcdc181c174351ab0f339d0
roxanacruceanu <60388484+roxanacruceanu@users.noreply.github.com>
2020-04-10 16:05:03 +0200

    Add files via upload

---
+++ servlet/Home.java
@@ -0,0 +1,27 @@
+package servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/home")
+public class Home extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    public Home() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		request.getServletContext().getRequestDispatcher("/jsp/header.jsp").include(request, response);
+		request.getServletContext().getRequestDispatcher("/jsp/navbar.jsp").include(request, response);
+		request.getServletContext().getRequestDispatcher("/jsp/home.jsp").include(request, response);
+		request.getServletContext().getRequestDispatcher("/jsp/footer.jsp").include(request, response);
+	}
+
+
+}
