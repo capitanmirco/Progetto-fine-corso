93c7e4127a080ccf25b9700ddbfde34ab1c8dfc9
kemotx90 <60275739+kemotx90@users.noreply.github.com>
2020-04-13 16:09:37 +0200

    Update InterrompiNoleggiate.java

--- servlet/InterrompiNoleggiate.java
+++ servlet/InterrompiNoleggiate.java
@@ -1,12 +1,13 @@
 package servlet;
 
 import java.io.IOException;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import database.Database;
 import model.Auto;
@@ -21,7 +22,7 @@ public class InterrompiNoleggiate extends HttpServlet {
        
     }
 
-    // stato=0 --> in corso, stato=1 --> terminato, stato=2 --> noleggio interrotto
+    // stato=1 --> in corso, stato=0 --> terminato, stato=2 --> noleggio interrotto
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		if(request.getParameter("stato")!=null && request.getParameter("stato").equals("2") && request.getParameter("idNoleggio")!= null) {
@@ -31,10 +32,31 @@ public class InterrompiNoleggiate extends HttpServlet {
 			byte disponibile = 1;
 			byte interrotto = 2;
 			n.setStato(interrotto);
-			a.setDisponibilita(disponibile);
 			Database.getInstance().updateNoleggio(n);
-			Database.getInstance().updateAuto(a);
+			
+			if(!noleggiAttivi(a)) {
+				a.setDisponibilita(disponibile);
+				Database.getInstance().updateAuto(a);
+			}
+			
+			response.sendRedirect("listanoleggi");
 		}
+	}
+	
+	/*controlla se presenti altri noleggi per quel veicolo*/
+	public boolean noleggiAttivi(Auto a) {
+		boolean noleggiAttivi = false;
+		List<Noleggio> listaNoleggi = Database.getInstance().getListaNoleggi();
+		if(listaNoleggi!= null) {
+			for(Noleggio n : listaNoleggi) {
+				if(n.getAuto().getIdAuto() == a.getIdAuto() && n.getStato()==1) {
+					noleggiAttivi = true;
+				}
+			}
+		}
+		
+		return noleggiAttivi;
+		
 	}
 
 
