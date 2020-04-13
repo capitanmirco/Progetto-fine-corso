c652fcc73774eccaeda0ca5d9c414deddb86baf6
chiaramonak <50022531+chiaramonak@users.noreply.github.com>
2020-04-13 15:01:27 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -9,3 +9,18 @@ function mostraPatente(ruolo)
 		$('#patente').show();
 	}
 }
+
+function finestraLog(param)
+{
+	if(param=="accedi")
+	{
+		$('#login').css("display","block");
+		$('body>*:not("#login")').css('filter', 'blur(3px)');
+	}
+}
+//grayscale(80%)
+function esci()
+{
+	$('#login').css("display","none");
+	$('body>*').css('filter', 'none');
+}
