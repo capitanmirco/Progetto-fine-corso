fbc2645baff461c12d694ada5f7e304f78739b2e
federicomadeddu <60399073+federicomadeddu@users.noreply.github.com>
2020-04-14 12:09:53 +0200

    Update RipristinaPersona.java

--- servlet/RipristinaPersona.java
+++ servlet/RipristinaPersona.java
@@ -33,16 +33,16 @@ public class RipristinaPersona extends HttpServlet {
 				Utente u = Database.getInstance().getUtenteById(id);
 				u.setValidato(validato);
 				Database.getInstance().updateUtente(u);
-				request.getServletContext().getNamedDispatcher("gestionepersona").include(request, response);
-
+				response.sendRedirect("gestionepersona");
+				
 			} else if (request.getParameter("ripristinacliente") != null) {
 				int id = Integer.parseInt(request.getParameter("ripristinacliente"));
 
 				Cliente c = Database.getInstance().getClienteById(id);
 				c.setValidato(validato);
 				Database.getInstance().updateCliente(c);
-				request.getServletContext().getNamedDispatcher("gestionepersona").include(request, response);
-
+				response.sendRedirect("gestionepersona");
+				
 			}
 		}
 	}
