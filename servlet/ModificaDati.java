58b87b4bc0d4239975ca09c8ef05632d644821ef
kemotx90 <60275739+kemotx90@users.noreply.github.com>
2020-04-15 09:01:23 +0200

    Update ModificaDati.java

--- servlet/ModificaDati.java
+++ servlet/ModificaDati.java
@@ -11,7 +11,7 @@ import database.Database;
 import model.Cliente;
 import model.Utente;
 
-@WebServlet("/updatedati")
+@WebServlet("/modificadati")
 public class ModificaDati extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
