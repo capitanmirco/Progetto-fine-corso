6bb408013951055bee0473b774857bf1fadd7741
Eleonora412 <60390028+Eleonora412@users.noreply.github.com>
2020-04-15 17:27:10 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -24,21 +24,3 @@ function esci()
 	$('#divLogin').css("display","none");
 	$('body>*').css('filter', 'none');
 }
-
-
-function checkPassword(){
-	alert ('funziona');
-	var password = document.getElementById('exampleInputPassword');
-	var cPassword = document.getElementById('exampleInputConfermaPassword');
-	
-	if(password.value != cPassword.value){
-		alert('Le password non coincidono');
-		password.focus();
-		password.select();
-	} else {
-		alert(password.value);
-		alert(cPassword.value);
-		document.getQuerySelector(".registra>form").submit();
-
-	}
-}
