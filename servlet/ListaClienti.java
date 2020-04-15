8d8228f77ea7bbeba3db99ba0a50581af3e72c32
JuriBardella <60388476+JuriBardella@users.noreply.github.com>
2020-04-15 13:04:58 +0200

    Update ListaClienti.java

--- servlet/ListaClienti.java
+++ servlet/ListaClienti.java
@@ -17,41 +17,41 @@ import model.Cliente;
 @WebServlet(name="listaclienti", urlPatterns = {"/listaclienti"})
 public class ListaClienti extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	List<Cliente> listaClienti;
-	List<Cliente>listaNonValidati;
-	List<Cliente>listaValidati;
-	List<Cliente>listaCancellati;
-       
-    
-    public ListaClienti() {
-    }
-
-	
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    	listaNonValidati=new ArrayList<Cliente>();
-    	listaValidati=new ArrayList<Cliente>();
-    	listaCancellati=new ArrayList<Cliente>();
-    	listaClienti = Database.getInstance().getListaClienti();
-
-    	for (Cliente cliente : listaClienti) {
-    		if(cliente.getValidato()==0 && !listaNonValidati.contains(cliente)) {
-
-    			listaNonValidati.add(cliente);
-
-    		}else if(cliente.getValidato()==1 && !listaValidati.contains(cliente)) {
-
-    			listaValidati.add(cliente);
 
 
-    		}else if(cliente.getValidato()==2 && !listaCancellati.contains(cliente)) {
-    			listaCancellati.add(cliente);
+	public ListaClienti() {
+	}
 
-    		}
-    	}
-    	request.setAttribute("listaClientiNonValidati", listaNonValidati);
-    	request.setAttribute("listaClientiValidati", listaValidati);
-    	request.setAttribute("listaClientiCancellati", listaCancellati);
 
-    }	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		List<Cliente> listaClienti= new ArrayList<Cliente>();
+		List<Cliente>listaNonValidati = new ArrayList<Cliente>();
+		List<Cliente>listaValidati =new ArrayList<Cliente>();
+		List<Cliente>listaCancellati =new ArrayList<Cliente>();
+
+
+		listaClienti = Database.getInstance().getListaClienti();
+
+		for (Cliente cliente : listaClienti) {
+			if(cliente.getValidato()==0 && !listaNonValidati.contains(cliente)) {
+
+				listaNonValidati.add(cliente);
+
+			}else if(cliente.getValidato()==1 && !listaValidati.contains(cliente)) {
+
+				listaValidati.add(cliente);
+
+
+			}else if(cliente.getValidato()==2 && !listaCancellati.contains(cliente)) {
+				listaCancellati.add(cliente);
+
+			}
+		}
+		request.setAttribute("listaClientiNonValidati", listaNonValidati);
+		request.setAttribute("listaClientiValidati", listaValidati);
+		request.setAttribute("listaClientiCancellati", listaCancellati);
+
+	}	
 
 }
