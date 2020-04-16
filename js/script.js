9e43ed974e2a7d7eb21426f347ecc38a1ba13bbd
chiaramonak <50022531+chiaramonak@users.noreply.github.com>
2020-04-16 15:40:23 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,26 +1,19 @@
 function mostraPatente(ruolo)
-{
-	if(ruolo=="utente")
 	{
-		$('#patente').hide();
+		if(ruolo=="utente")
+		{
+			$('#patente').hide();
+		}
+		else if (ruolo=="cliente")
+		{
+			$('#patente').show();
+		}
 	}
-	else if (ruolo=="cliente")
-	{
-		$('#patente').show();
-	}
-}
 
-function finestraLog(param)
-{
-	if(param=="accedi")
-	{
-		$('#divLogin').css("display","flex");
-		$('body>*:not("#divLogin")').css('filter', 'blur(3px)');
-	}
-}
+
 //grayscale(80%)
 function esci()
 {
 	$('#divLogin').css("display","none");
-	$('body>*').css('filter', 'none');
+	$('body>*,  #div-car, .bgselect.cal-position').css('filter', 'none');
 }
