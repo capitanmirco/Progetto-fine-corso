04c7849eeab9084cabb81c7f701f1228c88fbe63
federicomadeddu <60399073+federicomadeddu@users.noreply.github.com>
2020-04-16 22:59:35 +0200

    Update ModificaDati.java

--- servlet/ModificaDati.java
+++ servlet/ModificaDati.java
@@ -38,80 +38,136 @@ public class ModificaDati extends HttpServlet {
 				(request.getParameter("password") != null)&&(!request.getParameter("password").trim().equals(""))
 				&& (!request.getParameter("datadinascita").trim().equals(""))&& (request.getParameter("datadinascita") != null) &&
 				(!request.getParameter("codicefiscale").trim().equals(""))&&(request.getParameter("codicefiscale") != null)) {
-			
+
 			String nome = request.getParameter("nome");
 			String cognome = request.getParameter("cognome");
 			String email = request.getParameter("email");
 			String password = request.getParameter("password");
 			String dataDiNascita = request.getParameter("datadinascita");
 			String codiceFiscale = request.getParameter("codicefiscale");
-			
+
 			if (session.getAttribute("cliente") != null) {
+
 				Cliente c = (Cliente) session.getAttribute("cliente");
+				System.out.println("nome iniziale" + c.getNome());
+
 				String numeroPatente = request.getParameter("numeropatente");
+
 				if(!c.getNome().equals(nome)) {
 					c.setNome(nome);
 				}
+
 				if(!c.getCognome().equals(cognome)) {
 					c.setCognome(cognome);
 				}
+
 				if(!c.getPassword().equals(password)) {
 					c.setPassword(password);
 				}
+
+				if(!c.getEmail().equals(email)) {
+					Cliente cliente_temp = Database.getInstance().getCliente(email);
+					if(cliente_temp.getIdCliente()==(c.getIdCliente())) {
+						c.setEmail(email);
+					}
+					else {
+						request.setAttribute("erroremodificacliente", true);
+						System.out.println("email esistente");
+						doGet(request, response);
+					}
+				}
+
 				if(!c.getDataDiNascita().equals(dataDiNascita)) {
 					c.setDataDiNascita(dataDiNascita);
 				}
-				if(!c.getCodiceFiscale().equals(codiceFiscale)) {
-					c.setCodiceFiscale(codiceFiscale);
-				}
-				if(!c.getNumeroPatente().equals(numeroPatente)) {
-					c.setNumeroPatente(numeroPatente);
-				}
-				if (!(Database.getInstance().getClienteByCF(codiceFiscale) != null) && !(Database.getInstance().getCliente(email) != null) 
-						&& !(Database.getInstance().getClienteByPatente(request.getParameter("numeropatente")) != null)) {
-				
-					if(request.getParameter("numeropatente")!=null&&(!request.getParameter("numeropatente").trim().equals(""))) {
-						
 
-						Database.getInstance().updateCliente(c);
-						
+				if(!c.getCodiceFiscale().equals(codiceFiscale)) {
+					Cliente cliente_temp = Database.getInstance().getClienteByCF(codiceFiscale);
+					if(cliente_temp.getIdCliente()==(c.getIdCliente())) {
+						c.setCodiceFiscale(codiceFiscale);
+					}
+					else {
+						request.setAttribute("erroremodificacliente", true);
+						System.out.println("codice fiscale esistente");
+						doGet(request, response);
+					}
+				}
+
+				if(!c.getNumeroPatente().equals(numeroPatente)) {
+					if((request.getParameter("numeropatente")!=null) && (!request.getParameter("numeropatente").trim().equals(""))) {
+						Cliente cliente_temp = Database.getInstance().getClienteByPatente(numeroPatente);
+						if(cliente_temp.getIdCliente()==(c.getIdCliente())) {
+							c.setNumeroPatente(numeroPatente);
+						}
+						else {
+							request.setAttribute("erroremodificacliente", true);
+							System.out.println("patente esistente");
+							doGet(request, response);
+						}
+
+					}
+				}
+
+				//System.out.println("nome cambiato" +c.getNome());
+
+				if(request.getAttribute("erroremodificacliente") == null) {
+					Database.getInstance().updateCliente(c);
+					System.out.println("cliente modificato");
+					response.sendRedirect("visualizzadati");
+				}
+
+
+			}
+
+
+			if (session.getAttribute("utente") != null) {
+
+				Utente u = (Utente) session.getAttribute("utente");
+
+					if(!u.getNome().equals(nome)) {
+						u.setNome(nome);
+					}
+					if(!u.getCognome().equals(cognome)) {
+						u.setCognome(cognome);
+					}
+					if(!u.getEmail().equals(email)) {
+						Utente utente_temp = Database.getInstance().getUtente(email);
+						if(utente_temp.getIdUtente()==(u.getIdUtente())) {
+							u.setEmail(email);
+						}
+						else {
+							request.setAttribute("erroremodificautente", true);
+							System.out.println("email esistente");
+							doGet(request, response);
+						}
+					}
+					if(!u.getPassword().equals(password)) {
+						u.setPassword(password);
+					}
+					if(!u.getDataDiNascita().equals(dataDiNascita)) {
+						u.setDataDiNascita(dataDiNascita);
+					}
+					if(!u.getCodiceFiscale().equals(codiceFiscale)) {
+						Utente utente_temp = Database.getInstance().getUtenteByCF(codiceFiscale);
+						if(utente_temp.getIdUtente()==(u.getIdUtente())) {
+							u.setEmail(email);
+						}
+						else {
+							request.setAttribute("erroremodificautente", true);
+							System.out.println("email esistente");
+							doGet(request, response);
+						}
+					}
+
+					if(request.getAttribute("erroremodificautente") == null) {
+						Database.getInstance().updateUtente(u);
+						System.out.println("utente modificato");
 						response.sendRedirect("visualizzadati");
 					}
 				}
-				else {
-					request.setAttribute("erroremodificacliente", "si");
-					System.out.println("hai scritto qualcosa di brutto");
-					doGet(request, response);
-				}
-				
+
 			}
-			
 
-			if (session.getAttribute("utente") != null) {
-				
-				if (!(Database.getInstance().getUtenteByCF(codiceFiscale) != null) && !(Database.getInstance().getUtente(email) != null)) {
-
-				Utente u = (Utente) session.getAttribute("utente");
-				
-
-				u.setNome(nome);
-				u.setCognome(cognome);
-				u.setEmail(email);
-				u.setPassword(password);
-				u.setDataDiNascita(dataDiNascita);
-				u.setCodiceFiscale(codiceFiscale);
-
-				Database.getInstance().updateUtente(u);
-				response.sendRedirect("visualizzadati");
-			}
-				else {
-					request.setAttribute("erroremodificautente", "si");
-					System.out.println("hai scritto qualcosa di brutto");
-					doGet(request, response);
-				}
 		}
-		
 	}
 
-}
-}
